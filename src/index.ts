d694b53fe2bc7e74944120180aff6526328b89b8
tom <toms@live.jp>
2022-08-29 17:20:41 +0900

    SecretKey class is added

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export class SecretKey {
+  protected password: string;
+  protected salt: string;
+  protected exposedName: string;
+  constructor(password: string, salt: string, exposedName?: string){
+    this.password = password;
+    this.salt = salt;
+    this.exposedName = exposedName || "electronade";
+  }
+  public async encode(plainText: string){
+    // @ts-ignore
+    return globalThis[this.exposedName].endecoder.encode({
+      plainText,
+      password: this.password,
+      salt: this.salt
+    });
+  }
+  public async decode(encodedText: string){
+    // @ts-ignore
+    return globalThis[this.exposedName].endecoder.decode({
+      encodedText,
+      password: this.password,
+      salt: this.salt
+    });
+  }
+}
