8736bba06bc83f40117326b93536fb013b25bed4
tom <toms@live.jp>
2022-09-29 17:31:48 +0900

    format:src

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,41 @@ export class SecretKey {
   protected password: string;
   protected salt: string;
   protected exposedName: string;
-  constructor(password: string, salt: string, exposedName?: string){
+  constructor(password: string, salt: string, exposedName?: string) {
     this.password = password;
     this.salt = salt;
     this.exposedName = exposedName || "electronade";
   }
-  public async encode(plainText: string){
+  public async encode(plainText: string) {
     // @ts-ignore
     return globalThis[this.exposedName].endecoder.encode({
       plainText,
       password: this.password,
-      salt: this.salt
+      salt: this.salt,
     });
   }
-  public async decode(encodedText: string){
+  public async decode(encodedText: string) {
     // @ts-ignore
     return globalThis[this.exposedName].endecoder.decode({
       encodedText,
       password: this.password,
-      salt: this.salt
+      salt: this.salt,
     });
   }
-  public async encrypt(plainBuffer: Buffer){
+  public async encrypt(plainBuffer: Buffer) {
     // @ts-ignore
     return globalThis[this.exposedName].endecoder.encrypt({
       plainBuffer,
       password: this.password,
-      salt: this.salt
+      salt: this.salt,
     });
   }
-  public async decrypt(encodedText: string){
+  public async decrypt(encodedText: string) {
     // @ts-ignore
     return globalThis[this.exposedName].endecoder.decrypt({
       encodedText,
       password: this.password,
-      salt: this.salt
+      salt: this.salt,
     });
   }
 }
